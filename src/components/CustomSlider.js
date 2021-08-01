6928560c338b13b047e1abde610d165187394736
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2021-08-01 19:28:18 +0500

    fix slider

--- src/components/CustomSlider.js
+++ src/components/CustomSlider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Slider } from 'react-semantic-ui-range';
 import 'semantic-ui-css/semantic.min.css';
 import { Label } from 'semantic-ui-react';
@@ -8,13 +8,21 @@ import { useSliderSettings } from '../context/sliderSettingsContext';
 const CustomSlider = () => {
   const { state: filters, actions } = useFilters();
   const { settings: sliderSettings } = useSliderSettings();
-
+  const [showSlider, setShowSlider] = useState(false);
   const settings = {
     ...sliderSettings,
+    step: (sliderSettings.max - sliderSettings.min) / 100,
     onChange: value => {
       actions.setVolume(value);
     },
   };
+
+  useEffect(() => {
+    setShowSlider(false);
+    setTimeout(() => {
+      setShowSlider(true);
+    }, 100);
+  }, [filters.blockSize, sliderSettings]);
 
   const [min, max] = filters.volume;
 
@@ -25,12 +33,14 @@ const CustomSlider = () => {
           ? 'Market Cap Range'
           : 'Volume Size Range'}
         :{parseFloat(min).toFixed(2)}B - {parseFloat(max).toFixed(2)}B
-        <Slider
-          multiple
-          color="black"
-          settings={settings}
-          value={filters.volume}
-        />
+        {showSlider ? (
+          <Slider
+            multiple
+            color="black"
+            settings={settings}
+            value={filters.volume}
+          />
+        ) : null}
       </Label>
     </>
   );
